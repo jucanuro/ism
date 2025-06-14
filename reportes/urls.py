7403613149312e684ff18c781b9e0beba0fec12d
jucanuro <juankanunezr@gmail.com>
2025-06-14 15:56:18 -0500

    Cambios 14-06-2025

--- reportes/urls.py
+++ reportes/urls.py
@@ -1,3 +1,4 @@
+# En tu archivo urls.py de tu aplicación
 from django.urls import path
 from . import views
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('rechazos/', views.reporte_rechazos_view, name='reporte_rechazos_view'),
     path('rechazos/exportar-excel/', views.export_rechazos_excel_view, name='exportar_rechazos_excel'),
     path('actualizar-datos/', views.actualizar_datos_view, name='actualizar_datos'),
+    path('exportar-ventas-y-rechazos-juntos/', views.export_ventas_y_rechazos_juntos_excel_view, name='exportar_ventas_y_rechazos_juntos'),
 ]
